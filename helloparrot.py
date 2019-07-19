ab02c9878fc003615284ed364e04112100dd7e01
Ryan <ryan.glaeser@me.com>
2019-07-18 19:41:58 -0700

    The parrot of wisdom has spoken

---
+++ helloparrot.py
@@ -0,0 +1,9 @@
+import random
+
+print('The parrot looks at you expectantly.\n')
+a = input('You say, for some reason, to the parrot: \n')
+x = random.randint(0,2)
+sayings = ['Arrrr, ','Ahoy, ','Squaaack, ']
+y = sayings[x]
+
+print('\n' + "The parrot of wisdom says '" + y + a + "'")
